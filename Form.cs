46bcdb7d1601fa89aec9fa8dcb8f47c6ec2ac3cf
Dell <Dell@DESKTOP-O7TQ14S>
2024-04-14 13:32:46 -0400

    Add project files.

---
+++ Form.cs
@@ -0,0 +1,6 @@
+﻿namespace UserProfileApp
+{
+    public class Form
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+namespace UserProfileApp
+{
+    public partial class UserProfileForm : Form
+    {
+        private object txtEmail;
+        private object txtName;
+        private object txtPhoneNumber;
+        private object txtAddress;
+        private object txtCourseName;
+        private string txtPassword;
+
+        public UserProfileForm()
+        {
+            InitializeComponent();
+            LoadUserProfile();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadUserProfile()
+        {
+            // Load user profile details
+            string email = "john@graph.com";
+            string name = "Jon Doe";
+            string phoneNumber = "+1 xxx-xxx-xxxx";
+            string address = "123, ABC, Kitchener, ON";
+            string courseName = "[Course Name]";
+            string password = "*********";
+
+            // Display user profile details in the form
+            txtEmail = email;
+            txtName = name;
+            txtPhoneNumber = phoneNumber;
+            txtAddress = address;
+            txtCourseName = courseName;
+            txtPassword = password;
+        }
+    }
+}
